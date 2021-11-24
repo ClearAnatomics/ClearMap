9ccecb2e3d1418b823c8c4b6f06cb1b4fac5cf6c
crousseau <charly.rousseau@icm-institute.org>
2021-11-24 13:50:44 +0100

    Fixes trailing semicolons in DataViewer

--- ClearMap/Visualization/Qt/DataViewer.py
+++ ClearMap/Visualization/Qt/DataViewer.py
@@ -456,11 +456,11 @@ class DataViewer(pg.QtGui.QWidget):
     self.updateLabel()
 
   def updateLabel(self):
-    x,y,z = self.source_pointer;
-    xs,ys,zs = self.source_scale;
-    vals = ", ".join([str(s[x,y,z]) for s in self.sources]);
     self.source_label.setText("<span style='font-size: 12pt; color: black'>(%d, %d, %d) {%.2f, %.2f, %.2f} [%s]</span>" % (x,y,z,x*xs,y*ys,z*zs,vals))
-  
+    x,y,z = self.source_pointer
+    xs,ys,zs = self.source_scale
+    vals = ", ".join([str(s[x,y,z]) for s in self.sources])
+
   def updateSlice(self):
     ax = self.source_axis
     index = min(max(0, int(self.sliceLine.value())), self.source_shape[ax]-1)
