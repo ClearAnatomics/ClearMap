784208f569f625ede858c19cd72f7b5ff535c90a
crousseau <charly.rousseau@icm-institute.org>
2021-12-13 14:11:53 +0100

    Creates nested progress dialog and moves graphics resources

--- ClearMap/gui/dialogs.py
+++ ClearMap/gui/dialogs.py
@@ -1,10 +1,13 @@
+import os
 import sys
 
 from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QProgressDialog, QLabel
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QProgressDialog, QLabel, QDialogButtonBox
 
+from ClearMap.config.config_loader import clean_path
 from ClearMap.gui.gui_utils import runs_from_pycharm
+from ClearMap.gui.pyuic_utils import loadUiType
 
 
 def get_directory_dlg(start_folder, title="Choose the source directory"):
@@ -13,6 +16,7 @@ def get_directory_dlg(start_folder, title="Choose the source directory"):
         opt = QFileDialog.Options(QFileDialog.DontUseNativeDialog)
     else:
         opt = QFileDialog.Options()
+    start_folder = clean_path(start_folder)
     src_folder = diag.getExistingDirectory(parent=diag, caption=title,
                                            directory=start_folder, options=opt)
     diag.close()
@@ -32,14 +36,34 @@ def warning_popup(base_msg, msg):
 
 def make_progress_dialog(msg, maximum, canceled_callback, parent):
     dlg = QProgressDialog(msg, 'Abort', 0, maximum, parent=parent)  # TODO: see if can have a notnativestyle on unity
-    dlg.setMinimumDuration(0)
     dlg.setWindowTitle(msg)
     dlg.lbl = QLabel(msg, parent=dlg)
     # dlg.lbl.setText(msg)  # TODO: check why this doesn't work with pixmap
-    dlg.lbl.setPixmap(QPixmap('ClearMap/gui/icons/searching_mouse.png'))
+    dlg.lbl.setPixmap(QPixmap('ClearMap/gui/graphics_resources/searching_mouse.png'))
     dlg.lbl.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
     dlg.setLabel(dlg.lbl)
     if canceled_callback is not None:
         dlg.canceled().connect(canceled_callback)
-    dlg.setValue(0)  # To force update
+    dlg.setMinimumDuration(0)
+    dlg.forceShow()  # To force update
+    return dlg
+
+
+def make_nested_progress_dialog(title='Processing', overall_maximum=100, sub_maximum=100, sub_process_name='',
+                                abort_callback=None, parent=None):
+    dlg_class, _ = loadUiType('ClearMap/gui/nested_progress_dialog.ui', patch_parent_class='QDialog')
+    dlg = dlg_class()
+    dlg.setWindowTitle('Progress')
+    dlg.setupUi()
+    dlg.mainLabel.setText(title)
+    dlg.progressImageLabel.setPixmap(QPixmap('ClearMap/gui/graphics_resources/searching_mouse.png'))  # TODO: why doesn't work w/ qrc ??
+    dlg.mainProgressBar.setMinimum(1)
+    dlg.mainProgressBar.setValue(1)  # Because we use integer steps
+    dlg.mainProgressBar.setMaximum(overall_maximum)
+    dlg.subProgressLabel.setText(sub_process_name)
+    dlg.subProgressBar.setMaximum(sub_maximum)
+    dlg.subProgressBar.setValue(0)
+    if abort_callback is not None:
+        dlg.buttonBox.button(QDialogButtonBox.Abort).clicked.connect(abort_callback)
+    dlg.show()  # TODO: add onMaximumReached dlg.close binding
     return dlg
