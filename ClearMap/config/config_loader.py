555a56507e2a65b16e75b3e846cac47008f090be
crousseau <charly.rousseau@icm-institute.org>
2022-05-11 10:48:24 +0200

    Minor refactoring

--- ClearMap/config/config_loader.py
+++ ClearMap/config/config_loader.py
@@ -8,8 +8,8 @@ import configobj
 # TODO: add options to read different config file types (i.e. yaml and json)
 def get_configs(cfg_path, processing_params_path, machine_cfg_path='~/.clearmap/machine_params.cfg'):
     sample_config = get_configobj_cfg(cfg_path)
-    machine_config = get_configobj_cfg(machine_cfg_path)
     processing_config = get_configobj_cfg(processing_params_path)
+    machine_config = get_configobj_cfg(machine_cfg_path)
 
     return machine_config, sample_config, processing_config
 
