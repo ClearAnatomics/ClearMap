5a86321ccd71892cf036c3b6370cc5ee4707d0af
crousseau <charly.rousseau@icm-institute.org>
2022-09-26 15:42:31 +0200

    Fixes combo boxes not displaying contents correctly

--- ClearMap/gui/style.py
+++ ClearMap/gui/style.py
@@ -43,3 +43,58 @@ QToolTip {{
     border-radius: 3px;
     opacity: 200;
 }}"""
+
+# QComboBox::down-arrow {{
+#     image: url(/usr/share/icons/crystalsvg/16x16/actions/1downarrow.png);
+# }}
+
+
+#
+COMBOBOX_STYLE_SHEET = f"""
+QComboBox {{
+    border: 1px solid {DarkPalette.COLOR_BACKGROUND_5};
+    border-radius: 3px;
+    padding: 1px 2px 1px 3px;
+    min-width: 2em;
+}}
+
+/* QComboBox gets the "on" state when the popup is open */
+QComboBox:!editable:on, QComboBox::drop-down:editable:on {{
+    background: {DarkPalette.COLOR_BACKGROUND_3};
+}}
+
+QComboBox:editable {{
+    background: {DarkPalette.COLOR_BACKGROUND_3};
+}}
+
+QComboBox:!editable, QComboBox::drop-down:editable {{
+     background: {DarkPalette.COLOR_BACKGROUND_3};
+}}
+
+QComboBox:on {{ /* shift the text when the popup opens */
+     padding-top: 3px;
+     padding-left: 4px;
+}}
+
+QComboBox::down-arrow:on {{ /* shift the arrow when popup is open */
+    top: 1px;
+    left: 1px;
+}}
+
+QComboBox::drop-down {{
+    subcontrol-origin: padding;
+    subcontrol-position: top right;
+    width: 15px;
+
+    border-left-width: 1px;
+    border-left-color: darkgray;
+    border-left-style: solid; /* just a single line */
+    border-top-right-radius: 3px; /* same radius as the QComboBox */
+    border-bottom-right-radius: 3px;
+}}
+
+QComboBox QAbstractItemView {{
+    border: 2px solid {DarkPalette.COLOR_ACCENT_2};
+    selection-background-color: pink;
+}}
+"""
