4b6b82fadd704cc62d4aa48cd97c26c949f8d516
crousseau <charly.rousseau@icm-institute.org>
2022-10-19 14:18:10 +0200

    Fixes style to fully use QDarkStyle

--- ClearMap/gui/style.py
+++ ClearMap/gui/style.py
@@ -18,20 +18,14 @@ QDARKSTYLE_BACKGROUND = '#2E3436'
 DARK_BACKGROUND = '#282D2F'
 VERY_LIGHT_BACKGROUND = '#727A7E'
 PLOT_3D_BG = '#1A1D1E'
-BTN_STYLE_SHEET = \
-    'QPushButton {'\
-    'background-color: #455364; '\
-    'color: #E0E1E3;'\
-    'border-radius: 4px;'\
-    'padding: 2px;'\
-    '}'\
-    'QPushButton:pressed {'\
-    'background-color: #259AE9; }'
-# 'QPushButton {'
-# 'outline: none;'
-# 'QPushButton:pressed {'\
-# 'background-color: #60798B; '
-# 'border: 2px #259AE9;'
+BTN_STYLE_SHEET = f"""
+QPushButton {{
+    background-color: {DarkPalette.COLOR_BACKGROUND_5};
+    color: white;
+    border: 2px white solid;
+    border-radius: 4px;
+    padding: 2px;
+}}"""
 
 TOOLTIP_STYLE_SHEET = f"""
 QToolTip {{
