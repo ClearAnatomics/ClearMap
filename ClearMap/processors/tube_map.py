ClearAnatomics/ClearMap

commit 95fb4682b78d684ed4aeaeb1b35a351cce462e90

adds hemisphere labelling on graphs

ClearMap/processors/tube_map.py
@@ -558,6 +558,18 @@ class VesselGraphProcessor(TabProcessor):
                                                vertex_properties={'coordinates_atlas': 'annotation'},
                                                edge_geometry_properties={'coordinates_atlas': 'annotation'})
 
+	
+	def annotation_hemisphere(coordinates):
+            hemisphere_labels = annotation_module.label_points(coordinates, 
+            							annotation_file=self.preprocessor.hemispheres_file_path,
+            							key='id')
+            return hemisphere_labels
+	
+	self.graph_reduced.annotate_properties(annotation_hemisphere,
+                                               vertex_properties={'coordinates_atlas': 'hemisphere'},
+                                               edge_geometry_properties={'coordinates_atlas': 'hemisphere'})
+                                               
+
     def _compute_distance_to_surface(self):
         """add distance to brain surface as vertices properties"""
         # %% Distance to surface

commit 39ad33cebaea9830d0575cfd410eacc98bd99ac7

Fixes indentation issues introduced in tube_map by commit 95fb4682b7

ClearMap/processors/tube_map.py
@@ -554,21 +554,19 @@ class VesselGraphProcessor(TabProcessor):
             label = annotation_module.label_points(coordinates, key='id')
             return label
 
+        def annotation_hemisphere(coordinates):
+            hemisphere_labels = annotation_module.label_points(coordinates,
+                                                               annotation_file=self.preprocessor.hemispheres_file_path,
+                                                               key='id')
+            return hemisphere_labels
+
         self.graph_reduced.annotate_properties(annotation,
                                                vertex_properties={'coordinates_atlas': 'annotation'},
                                                edge_geometry_properties={'coordinates_atlas': 'annotation'})
 
-	
-	def annotation_hemisphere(coordinates):
-            hemisphere_labels = annotation_module.label_points(coordinates, 
-            							annotation_file=self.preprocessor.hemispheres_file_path,
-            							key='id')
-            return hemisphere_labels
-	
-	self.graph_reduced.annotate_properties(annotation_hemisphere,
+        self.graph_reduced.annotate_properties(annotation_hemisphere,
                                                vertex_properties={'coordinates_atlas': 'hemisphere'},
                                                edge_geometry_properties={'coordinates_atlas': 'hemisphere'})
-                                               
 
     def _compute_distance_to_surface(self):
         """add distance to brain surface as vertices properties"""