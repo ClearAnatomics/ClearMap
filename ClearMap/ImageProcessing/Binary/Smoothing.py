aab11e1faa460f656b2e23d215f43f3a93e45873
crousseau <charly.rousseau@icm-institute.org>
2023-03-22 16:02:47 +0100

    Wraps "smooth_by_configuration_block" to catch errors in multiprocessing

--- ClearMap/ImageProcessing/Binary/Smoothing.py
+++ ClearMap/ImageProcessing/Binary/Smoothing.py
@@ -342,24 +342,28 @@ def smooth_by_configuration_block(source, iterations = 1, verbose = False):
   smoothed : array
     Thre smoothed binary array.
   """
-  if isinstance(source, io.src.Source):
-    smoothed = source.array;
-  else:
-    smoothed = source;
-  smoothed = np.asarray(smoothed, dtype='uint32');
-  ndim = smoothed.ndim;
-  
-  lut = np.asarray(initialize_lookup_table(verbose=verbose), dtype='uint32');
-  
-  for i in range(iterations):   
-    #index 
-    for axis in range(ndim):
-      kernel = t3d.index_kernel(axis=axis);
-      smoothed = ndi.correlate1d(smoothed, kernel, axis=axis, output='uint32', mode='constant', cval=0);
-    smoothed = lut[smoothed];
-   
-    if verbose:
-      print('Binary Smoothing: itertion %d / %d done!' % (i+1, iterations));
+  try:
+    if isinstance(source, io.src.Source):
+      smoothed = source.array;
+    else:
+      smoothed = source;
+    smoothed = np.asarray(smoothed, dtype='uint32');
+    ndim = smoothed.ndim;
+
+    lut = np.asarray(initialize_lookup_table(verbose=verbose), dtype='uint32');
+
+    for i in range(iterations):
+      # index
+      for axis in range(ndim):
+        kernel = t3d.index_kernel(axis=axis);
+        smoothed = ndi.correlate1d(smoothed, kernel, axis=axis, output='uint32', mode='constant', cval=0);
+      smoothed = lut[smoothed];
+
+      if verbose:
+        print(f'Binary Smoothing: itertion {i+1} / {iterations} done!', flush=True)
+  except Exception as err:
+    print(err, flush=True)
+    raise
   
   return np.asarray(smoothed, dtype=bool);
 
