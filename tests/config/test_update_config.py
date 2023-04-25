59db9b2892cce3aa653ede37c7b9a673f1b93ef7
crousseau <charly.rousseau@icm-institute.org>
2023-04-25 15:23:08 +0200

    Refactors test_update_config

--- tests/config/test_update_config.py
+++ tests/config/test_update_config.py
@@ -7,21 +7,27 @@ from ClearMap.config.config_loader import get_configobj_cfg
 from ClearMap.config.update_config import deep_merge_dicts, remove_extra_keys
 
 
+def get_cfg(cfg_name):
+    cfg = get_configobj_cfg(os.path.join(os.path.dirname(os.path.abspath(__file__)), cfg_name))
+    return dict(cfg)
+
+
 @pytest.fixture
 def old_alignment():
-    cfg = get_configobj_cfg(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pre_2.1.0_alignment_params.cfg'))
-    return dict(cfg)
+    return get_cfg('pre_2.1.0_alignment_params.cfg')
 
 
 @pytest.fixture
 def new_alignment():
-    cfg = get_configobj_cfg(os.path.join(os.path.dirname(os.path.abspath(__file__)), '2.1.0_alignment_params.cfg'))
-    return dict(cfg)
+    return get_cfg('2.1.0_alignment_params.cfg')
 
 
-# @pytest.parametrize()
-def test_deep_merge_dicts(old_alignment, new_alignment):
+@pytest.fixture
+def corrected_alignement():
+    return get_cfg('pre_2.1.0_fixed_alignment_params.cfg')
+
+
+def test_deep_merge_dicts(old_alignment, new_alignment, corrected_alignement):
     assert old_alignment != new_alignment
     merged = remove_extra_keys(deep_merge_dicts(copy.deepcopy(old_alignment), new_alignment), new_alignment)
-    loaded_merged = dict(get_configobj_cfg(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pre_2.1.0_fixed_alignment_params.cfg')))
-    assert merged == loaded_merged
+    assert merged == corrected_alignement
