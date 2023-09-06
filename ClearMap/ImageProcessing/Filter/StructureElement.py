0c37090d04f42a5d3f46104d4097ebc095a11920
crousseau <charly.rousseau@icm-institute.org>
2023-09-06 17:48:11 +0200

    STYLE: refactors StructureElement style

--- ClearMap/ImageProcessing/Filter/StructureElement.py
+++ ClearMap/ImageProcessing/Filter/StructureElement.py
@@ -5,77 +5,81 @@ StrutureElement
 
 Routines to generate structure elements for filters.
 """
-__author__    = 'Christoph Kirst <christoph.kirst.ck@gmail.com>'
-__license__   = 'GPLv3 - GNU General Pulic License v3 (see LICENSE.txt)'
+__author__ = 'Christoph Kirst <christoph.kirst.ck@gmail.com>'
+__license__ = 'GPLv3 - GNU General Public License v3 (see LICENSE.txt)'
 __copyright__ = 'Copyright © 2020 by Christoph Kirst'
-__webpage__   = 'http://idisco.info'
-__download__  = 'http://www.github.com/ChristophKirst/ClearMap2'
+__webpage__ = 'https://idisco.info'
+__download__ = 'https://www.github.com/ChristophKirst/ClearMap2'
+
 
 import numpy as np
 
 
-def disk(shape = (3,3)):
-  """Disk structuring element."""
-  return sphere(shape = shape) > 0;
+def disk(shape=(3, 3)):
+    """Disk structuring element."""
+    return sphere(shape=shape) > 0
 
 
-def sphere(shape = (3,3)):
-  """Disk structuring element."""
-  offsets = structure_element_offsets(shape);
-  mesh = [range(-o[0], o[1]) for o in offsets];         
-  mesh = np.array(np.meshgrid(*mesh, indexing = 'ij'), dtype = float);
-                 
-  add = ((np.array(shape) + 1) % 2) / 2.0;   
-  nrm = np.max(offsets, axis=1);  
-  for d in range(len(add)):
-    mesh[d] = (mesh[d] + add[d]) / nrm[d];
-  
-  r = 1 - np.sum(mesh * mesh, axis = 0);
-  r[r < 0] = 0;
-  r /= r.sum();
-  return r;
+def sphere(shape=(3, 3)):
+    """Disk structuring element."""
+    offsets = structure_element_offsets(shape)
+    mesh = [range(-o[0], o[1]) for o in offsets]
+    mesh = np.array(np.meshgrid(*mesh, indexing='ij'), dtype=float)
+
+    add = ((np.array(shape) + 1) % 2) / 2.0
+    nrm = np.max(offsets, axis=1)
+    for d in range(len(add)):
+        mesh[d] = (mesh[d] + add[d]) / nrm[d]
+
+    r = 1 - np.sum(mesh * mesh, axis=0)
+    r[r < 0] = 0
+    r /= r.sum()
+    return r
 
 
-def cube(shape = (3,3)):
-  """Cube structuring element."""
-  return np.ones(shape, dtype = bool);
+def cube(shape=(3, 3)):
+    """Cube structuring element."""
+    return np.ones(shape, dtype=bool)
 
 
-def structure_element(shape = (3,3), form = 'Disk', ndim = None):
+def structure_element(shape=(3, 3), form='Disk', ndim=None):
     """Creates specific 2d and 3d structuring elements
       
     Arguments
     ---------
     shape : array or tuple
-      Shape of the structure element.
+      shape of the structure element
     form : str
       structure element type      
+    ndim: int or None
+      dimension of the structuring element
     
-    Returns:
+    Returns
+    -------
         array
             structure element
     """
     if isinstance(shape, int) and ndim is not None:
-      shape = (shape,) * ndim;
-    
-    if isinstance(shape, (list, tuple)):
-      shape = np.array([shape]).flatten();   
-      
-      if ndim is None:
-        ndim = len(shape);
-      else:
-        shape = np.pad(shape[:ndim], (0, max(0, ndim - len(shape))), 'wrap');
-      
-      if form in ['Disk', 'disk', 'd']:
-        return disk(shape = shape);
-      elif form in ['Sphere', 'shpere', 's']:
-        return sphere(shape = shape);
-      elif form in ['Cube', 'cube', 'c', 'Rectangle', 'rectangle', 'r']:
-        return cube(shape = shape);
-      else:
-        ValueError('Form %r for structuring element not valid!' % form);
+        shape = (shape,) * ndim
+
+    if isinstance(shape, (list, tuple)):  # FIXME: iterable
+        shape = np.array([shape]).flatten()
+
+        if ndim is None:
+            ndim = len(shape)
+        else:
+            shape = np.pad(shape[:ndim], (0, max(0, ndim - len(shape))), 'wrap')
+
+        if form in ['Disk', 'disk', 'd']:
+            return disk(shape=shape)
+        elif form in ['Sphere', 'shpere', 's']:
+            return sphere(shape=shape)
+        elif form in ['Cube', 'cube', 'c', 'Rectangle', 'rectangle', 'r']:
+            return cube(shape=shape)
+        else:
+            ValueError(f'Form {form} for structuring element not valid!')
     else:
-      return shape;                  
+        return shape
 
 
 def structure_element_offsets(shape):
@@ -91,23 +95,22 @@ def structure_element_offsets(shape):
     offsets : array
       Offsets to center taking care of even/odd number of elements.
     """
-    off = np.array([shape], dtype = int).flatten() // 2;
-    off = np.array([off, shape - off]).T;
-    return off;   
+    off = np.array([shape], dtype=int).flatten() // 2
+    off = np.array([off, shape - off]).T
+    return off
 
 
-###############################################################################
-### Tests
-############################################################################### 
+########################################################################################################################
+# Tests
+########################################################################################################################
 
 def _test():
-  import ClearMap.ImageProcessing.Filter.StructureElement as se
+    import ClearMap.ImageProcessing.Filter.StructureElement as se
   
-  from importlib import reload
-  reload(se)
+    from importlib import reload
+    reload(se)
 
-  d = se.sphere((150,50,10));  
-  
-  import ClearMap.Visualization.Plot3d as p3d
-  p3d.plot(d)             
+    element = se.sphere((150, 50, 10))
 
+    import ClearMap.Visualization.Plot3d as p3d
+    p3d.plot(element)
