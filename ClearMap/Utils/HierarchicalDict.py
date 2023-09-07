25759c0fda7ee103c682c89ed3d781241cd3ca9d
crousseau <charly.rousseau@icm-institute.org>
2023-09-07 16:08:21 +0200

    STYLE: Cleanup of HierarchicalDict

--- ClearMap/Utils/HierarchicalDict.py
+++ ClearMap/Utils/HierarchicalDict.py
@@ -9,7 +9,7 @@ Example
 -------
 
 >>> import ClearMap.Utils.HierarchicalDict as hdict
->>> d = dict(x = 10, y = 100, z = dict(a = 10, b = 20));
+>>> d = dict(x = 10, y = 100, z = dict(a = 10, b = 20))
 >>> print(hdict.get(d, 'z_a'))
 10
 
@@ -22,24 +22,23 @@ z: dict
    b: 20
    c: dict
       q: 42
-
-
 """
-__author__    = 'Christoph Kirst <christoph.kirst.ck@gmail.com>'
-__license__   = 'GPLv3 - GNU General Pulic License v3 (see LICENSE)'
+__author__ = 'Christoph Kirst <christoph.kirst.ck@gmail.com>'
+__license__ = 'GPLv3 - GNU General Public License v3 (see LICENSE)'
 __copyright__ = 'Copyright © 2020 by Christoph Kirst'
-__webpage__   = 'http://idisco.info'
-__download__  = 'http://www.github.com/ChristophKirst/ClearMap2'
+__webpage__ = 'https://idisco.info'
+__download__ = 'https://www.github.com/ChristophKirst/ClearMap2'
 
-
-DELIMITER = '_';
 
 from collections import OrderedDict as odict
 
-#class HierarchicalDict(dict):
-##  pass
 
-def get(parameter, key, default = None):
+DELIMITER = '_'
+# TODO: Could be handled more professionally by Enum Flag to allow for multiple parameter flags if needed
+# TODO: use a HierarchicalDict class -> derive ParameterDict for ClearMap use in GUI
+
+
+def get(parameter, key, default=None):
     """Gets a parameter from a dict, returns default value if not defined
     
     Arguments
@@ -58,23 +57,23 @@ def get(parameter, key, default = None):
     """
 
     if not isinstance(parameter, dict):
-      return default;
+        return default
 
     if not isinstance(key, str):
-      return parameter.get(key, default);
+        return parameter.get(key, default)
 
-    p = parameter;    
+    p = parameter
     for k in key.split(DELIMITER):
-      if k in p.keys():
-        p = p[k];
-      else:
-        return default;
-    
-    return p;
+        if k in p.keys():
+            p = p[k]
+        else:
+            return default
 
-    
-def set(parameter, key = None, value = None, **kwargs):
-    """Sets a parameter in a hierarchical dicitionary.
+    return p
+
+
+def set(parameter, key=None, value=None, **kwargs):
+    """Sets a parameter in a hierarchical dictionary.
     
     Arguments
     ---------
@@ -93,34 +92,34 @@ def set(parameter, key = None, value = None, **kwargs):
       Parameter dictionary.
     """
     if key is None or value is None:
-      keys = kwargs.keys();
-      values = kwargs.values();
+        keys = kwargs.keys()
+        values = kwargs.values()
     else:
-      keys = [key];
-      values = [value];
-    
-    for k,v in zip(keys, values):
-      if not isinstance(k, str):
-        parameter[k] = v;
-      else:
-        p = parameter;
-        ks = k.split(DELIMITER);
-        for l in ks[:-1]:
-          if isinstance(p, dict):
-            if l in p.keys():
-              p = p[l];
-            else:
-              p[l] = {};
-              p = p[l];
-          else:
-            raise RuntimeError("set: %s is not a dictionary!" % k);
-        
-        p[ks[-1]] = v;
-    
-    return parameter;
+        keys = [key]
+        values = [value]
+
+    for k, v in zip(keys, values):
+        if not isinstance(k, str):
+            parameter[k] = v
+        else:
+            p = parameter
+            ks = k.split(DELIMITER)
+            for l in ks[:-1]:
+                if isinstance(p, dict):
+                    if l in p.keys():
+                        p = p[l]
+                    else:
+                        p[l] = {}
+                        p = p[l]
+                else:
+                    raise RuntimeError(f"set: {k} is not a dictionary!")
+
+            p[ks[-1]] = v
+
+    return parameter
 
 
-def write(parameter = None, head = None, **kwargs):
+def write(parameter=None, head=None, **kwargs):
     """Writes parameter settings in a formatted way.
     
     Arguments
@@ -135,34 +134,32 @@ def write(parameter = None, head = None, **kwargs):
     Returns
     -------
     string : str
-      A formated string with parameter info.
+      A formatted string with parameter info.
     """
-    if head is None:
-      head = '';
-    elif len(head) > 0:
-      head = head + ' ';
-    
+    head = head or ''
+
+    if head:
+        head += ' '
+
     if parameter is None:
-      parameter = odict();
-    
-    parameter = join(parameter, kwargs);
-        
-    keys = parameter.keys();
-    vals = parameter.values();
-    parsize = max([len(x) for x in keys]);
-    
-    s = [];
-    for k,v in zip(keys, vals):
-      if isinstance(v, dict):
-        s.append(head + k.ljust(parsize) + ': dict')
-        s.append(write(v, head = ' ' * (len(head) + parsize) + ' '));
-      else:
-        s.append(head + k.ljust(parsize) + ': ' + str(v)); 
-    
+        parameter = odict()
+
+    parameter = join(parameter, kwargs)
+
+    par_size = max([len(x) for x in parameter.keys()])
+
+    s = []
+    for k, v in parameter.items():
+        if isinstance(v, dict):
+            s.append(f'{head}{k.ljust(par_size)}: dict')
+            s.append(write(v, head=' ' * (len(head) + par_size) + ' '))
+        else:
+            s.append(f'{head}{k.ljust(par_size)}: {v}')
+
     return '\n'.join(s)
 
 
-def pprint(parameter = None, head = None, **args):
+def pprint(parameter=None, head=None, **args):
     """Prints parameter settings in a formatted way.
     
     Arguments
@@ -174,29 +171,30 @@ def pprint(parameter = None, head = None, **args):
     args
       Additional parameter values as key=value arguments.
     """
-    print(write(parameter = parameter, head = head, **args));
+    print(write(parameter=parameter, head=head, **args))
 
-    
+
 def join(*args):
-    """Joins dictionaries in a consitent way
+    """Joins dictionaries in a consistent way
     
     Arguments
     ---------
     args : dicts
-      The parameter dictonaries to join.
+      The parameter dictionaries to join.
     
     Returns
     -------
     join : dict
         The joined dictionary.
     """
-    new = args[0];    
+    new = args[0]
     for add in args[1:]:
-      for k,v in add.items():
-        new[k] = v;
-        
-    return new;
-   
+        for k, v in add.items():
+            new[k] = v
+
+    return new
+
+
 #    keyList = [x.keys() for x in args];
 #    n = len(args);
 #    
@@ -210,20 +208,21 @@ def join(*args):
     
     
 def prepend(parameter, key):
-    """Adds a hierarchical key infront of all the parameter keys in a dictionary.
+    """Adds a hierarchical key in front of all the parameter keys in a dictionary.
 
     Arguments
     ---------
     parameter : dict
-      Parameter dictonary.
+      Parameter dictionary.
     key : str
-      Key to add infronat of the dictionary keys.
+      Key to add in front of the dictionary keys.
     
     Returns
     -------
     prepend : dict
-      The dictionary with modified keys.
+      New dictionary with modified keys.
     """
+    return {f'{key}{DELIMITER}{k}': v for k, v in parameter.items()}
     
     keys   = parameter.keys() 
     values = parameter.values();
@@ -233,14 +232,13 @@ def prepend(parameter, key):
         
 
 ###############################################################################
-### Tests
+# Tests
 ###############################################################################
 
 def _test():
-  import ClearMap.Utils.HierarchicalDict as hdict
+    import ClearMap.Utils.HierarchicalDict as hdict
   
-
-  d = dict(x = 10, y = 100, z = dict(a = 10, b = 20));
-  print(hdict.get(d, 'z_a'))
-  hdict.set(d, 'z_c_q', 42)
-  hdict.pprint(d)
+    d = dict(x=10, y=100, z=dict(a=10, b=20))
+    print(hdict.get(d, 'z_a'))
+    hdict.set(d, 'z_c_q', 42)
+    hdict.pprint(d)
