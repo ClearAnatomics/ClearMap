f7988d5c7e7b3ef3e17276e8ba0350d7103d88f6
crousseau <charly.rousseau@icm-institute.org>
2023-09-26 14:55:44 +0200

    STYLE: mostly typos

--- ClearMap/IO/GT.py
+++ ClearMap/IO/GT.py
@@ -14,7 +14,7 @@ See also
 :mod`ClearMap.Analysis.Graphs`
 """
 __author__    = 'Christoph Kirst <christoph.kirst.ck@gmail.com>'
-__license__   = 'GPLv3 - GNU General Pulic License v3 (see LICENSE.txt)'
+__license__   = 'GPLv3 - GNU General Public License v3 (see LICENSE.txt)'
 __copyright__ = 'Copyright © 2020 by Christoph Kirst'
 __webpage__   = 'http://idisco.info'
 __download__  = 'http://www.github.com/ChristophKirst/ClearMap2'
