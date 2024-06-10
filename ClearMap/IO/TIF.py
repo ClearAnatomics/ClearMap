ClearAnatomics/ClearMap

commit 75311a5e34a16f66e34c5c8b371147191d09e56e

IMPROVEMENT: Adds test for multifile support in current TIFFFILE lib

ClearMap/IO/TIF.py
@@ -33,7 +33,10 @@ class Source(src.Source):
   Its assumed that the image data is stored in a serregionies of the tif file.
   """
   def __init__(self, location, series = 0, multi_file = False):
-    self._tif = tif.TiffFile(location)
+    try:
+      self._tif = tif.TiffFile(location, multi_file=multi_file)
+    except TypeError:  # TODO: filter with message
+      self._tif = tif.TiffFile(location)
     self._series = series
     self.multi_file = multi_file
 
@@ -284,7 +287,7 @@ def is_tif(source):
   if isinstance(source, str):
     try:
       Source(source)
-    except :
+    except:  # FIXME: too broad
       return False
     return True
   return False

ClearMap/IO/GT.py
@@ -28,6 +28,7 @@ import ClearMap.IO.Source as src
 ### Source classe
 ###############################################################################
 
+
 class Source(src.Source):
   """GT graph source."""
   

commit 0a66051e5259c5f6bd8f7cfd42f373ffa08e958a

BUGFIX: Fixes regression introduced by 75311a5e34a16f66e34c5c8b371147191d09e56e

ClearMap/IO/TIF.py
@@ -34,9 +34,9 @@ class Source(src.Source):
   """
   def __init__(self, location, series = 0, multi_file = False):
     try:
-      self._tif = tif.TiffFile(location, multi_file=multi_file)
+      self._tif = tif.TiffFile(location, multifile=multi_file)
     except TypeError:  # TODO: filter with message
-      self._tif = tif.TiffFile(location)
+      self._tif = tif.TiffFile(location, _multifile=multi_file)  # Versions >= 2020.9.30
     self._series = series
     self.multi_file = multi_file
 