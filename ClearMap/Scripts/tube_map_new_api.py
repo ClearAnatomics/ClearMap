b7d3765a7826c74a061db948b8a152f1ec5b4afc
crousseau <charly.rousseau@icm-institute.org>
2024-07-26 12:30:22 +0200

    BUGFIX: fixes tube_map_new_api for arteries channel and removes automated plots

--- ClearMap/Scripts/tube_map_new_api.py
+++ ClearMap/Scripts/tube_map_new_api.py
@@ -9,46 +9,49 @@ optionally, provide the atlas base name as second argument
 import sys
 
 from ClearMap.Scripts.align_new_api import stitch, convert_stitched, register, plot_registration_results
+from ClearMap.config.atlas import ATLAS_NAMES_MAP
 from ClearMap.config.config_loader import ConfigLoader, get_configs
 from ClearMap.processors.sample_preparation import PreProcessor
 from ClearMap.processors.tube_map import BinaryVesselProcessor, VesselGraphProcessor
 
 
-def main(src_directory, atlas_base_name='ABA_25um_2017'):
+def main(src_directory):
     cfg_loader = ConfigLoader(src_directory)
     configs = get_configs(cfg_loader.get_cfg_path('sample'), cfg_loader.get_cfg_path('processing'))
     pre_proc = PreProcessor()
     pre_proc.setup(configs)
 
     stitch(pre_proc)
-    # if all(pre_proc.processing_config['stitching']['preview'].values)
-    pre_proc.plot_stitching_results()
+    # pre_proc.plot_stitching_results()
 
-    convert_stitched(pre_proc)
+    # convert_stitched(pre_proc)
 
+    atlas_id = pre_proc.processing_config['registration']['atlas']['id']
+    atlas_base_name = ATLAS_NAMES_MAP[atlas_id]['base_name']
     register(atlas_base_name, pre_proc)
-    plot_registration_results(pre_proc)
+    # plot_registration_results(pre_proc)
 
     binary_vessel_processor = BinaryVesselProcessor(pre_proc)
 
     binary_vessel_processor.setup(pre_proc)
+
     binary_vessel_processor.binarize()
-    binary_vessel_processor.plot_binarization_result(arrange=True)
-    binary_vessel_processor.plot_vessel_filling_results(arrange=True)
+    for channel in ('raw', 'arteries'):  # TODO: edit as required
+        binary_vessel_processor.smooth_channel(channel)
+        binary_vessel_processor.fill_channel(channel)
+        binary_vessel_processor.deep_fill_channel(channel)
+
     binary_vessel_processor.combine_binary()
-    binary_vessel_processor.plot_combined(arrange=True)
+    # binary_vessel_processor.plot_combined(arrange=True)
 
     vessel_graph_processor = VesselGraphProcessor(pre_proc)
     vessel_graph_processor.setup(pre_proc)
     vessel_graph_processor.pre_process()
-    # FIXME: slice
+    # TODO: slice
     vessel_graph_processor.post_process()
     vessel_graph_processor.voxelize()
-    vessel_graph_processor.plot_voxelization(None)
+    # vessel_graph_processor.plot_voxelization(None)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        main(sys.argv[1], sys.argv[2])
-    else:
-        main(sys.argv[1])
+    main(sys.argv[1])
