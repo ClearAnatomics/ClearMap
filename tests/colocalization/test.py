7b7faaf1227768e56ed93ed2d3bc1a44720501e3
Gaël Cousin <gcousin333@gmail.com>
2024-09-11 16:04:37 +0200

    advanced on colocalization module and added a test for one of its functions

---
+++ tests/colocalization/test.py
@@ -0,0 +1,66 @@
+import sys
+
+import numpy as np
+
+sys.path.insert(0, "../..")
+from ClearMap.Analysis.Measurements.maxima_detection import label_representatives
+from ClearMap.colocalization import bilabel_bincount
+
+
+### for label_representatives
+
+
+def rand_shape(dim):
+    return tuple(np.random.randint(15, size=(dim,)))
+
+
+def rand_array(dim, high):
+    size = rand_shape(dim)
+    return np.random.randint(high, size=size)
+
+
+def elem_assert(array):
+    res = label_representatives(array)
+    points = [tuple([a[k] for a in res]) for k in range(len(res[0]))]
+    vals = np.array([array[point] for point in points])
+
+    labels = np.unique(array)
+    if labels.size > 0 and labels[0] == 0:
+        labels = labels[1:]
+    information = f"labeled_array {array} \n output of label_reps:{res} \n corresponding tuples {points} \n there values {vals} \n vs all the labels  {labels}"
+    assert vals.size == labels.size, information
+    assert np.all(labels == vals), information
+
+
+def test_representatives():
+    for i in range(100):
+        array = rand_array(3, 10)
+        elem_assert(array)
+        elem_assert(array.transpose())
+
+
+### for bilabel_bin_count
+
+
+def test_bilabel_bincount():
+    for u in range(100):
+        A = np.random.randint(5, size=(20, 20))
+        B = np.random.randint(5, size=(20, 20))
+        counts = bilabel_bincount(A, B)
+        m = A.max() + 1
+        n = B.max() + 1
+        res = np.zeros((m, n), dtype="uint8")
+        for i in range(m):
+            for j in range(n):
+                res[i, j] = np.count_nonzero((A == i) * (B == j))
+        assert np.all(res == counts)
+
+
+# TODO: have this integrated within pytest framework
+
+if __name__ == "__main__":
+    print("testing label_representatives")
+    test_representatives()
+    print("testing bilabel_bincount")
+    test_bilabel_bincount()
+    print("The tests were successful!")
