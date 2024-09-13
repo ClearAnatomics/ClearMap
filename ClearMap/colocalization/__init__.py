b48ebc64963ea896fde7663f4f389febdb44cc09
Gaël Cousin <gcousin333@gmail.com>
2024-09-13 16:14:35 +0200

    enhanced the code with caching of overlaps

--- ClearMap/colocalization/__init__.py
+++ ClearMap/colocalization/__init__.py
@@ -1,3 +1,5 @@
+# Copyright Gaël Cousin & Charly Rousseau
+
 """
 Colocalization
 ----------------
@@ -107,7 +109,7 @@ def bilabel_bincount(labels_1: np.array, labels_2: np.array) -> np.array:
     return counts.reshape((max_1 + 1, max_2 + 1))
 
 
-# for comparison purposes
+# for comparison/test purposes
 def _naive_bilabel_bincount(A, B):
     m = A.max() + 1
     n = B.max() + 1
@@ -125,16 +127,32 @@ class Channel:
     where not all connected components have a representative but the points must belong to distinct components.
     """
 
+    channel_count = 0
+    channel_names = []
+
     def __init__(
         self,
         binary_img: np.ndarray,
         dataframe: pd.DataFrame,
         coord_names=["x", "y", "z"],
+        channel_name="",
     ) -> None:
         self.binary_img = binary_img
         self.dataframe = dataframe
         self.coord_names = coord_names
         self.ndim = len(self.coord_names)
+
+        # we identify the channel by a name fo caching management
+        if not channel_name:
+            channel_name = Channel.channel_count
+            Channel.channel_count += 1
+        if channel_name in Channel.channel_names:
+            raise ValueError("The passed Channel name is already in use.")
+
+        self.name = channel_name
+        Channel.channel_names.append(channel_name)
+
+        self._overlaps_dic = {}
 
     @property
     def representative_points(self):
@@ -147,26 +165,37 @@ class Channel:
         return labels
 
     @cached_property
-    def index_label_correspondance(self):
-        return [
-            self.labels[tuple(self.dataframe[self.coord_names].iloc[index])]
-            for index in range(len(self.dataframe))
-        ]
+    def index_label_correspondance(self) -> np.array:
+        """Return the labels of nuclei in the nuclei index order
+
+        Returns
+        -------
+        np.array
+            the flat array of the labels in the nuclei index order
+        """
+        return np.array(
+            [
+                self.labels[tuple(self.dataframe[self.coord_names].iloc[index])]
+                for index in range(len(self.dataframe))
+            ]
+        )
 
     @cached_property
     def label_index_correspondance(self):
         # beware that not all label corresponds to an index,
         # if we do not have a complete set of reps in the dataframe.
         # We set arbitrarily the index value 0 for the labels that will not be used.
-        max_label = max(self.index_label_correspondance)
-        return [
-            (
-                self.index_label_correspondance.index(label)
-                if label in self.index_label_correspondance
-                else 0
-            )
-            for label in range(max_label + 1)
-        ]
+        max_label = self.index_label_correspondance.max()
+        return np.array(
+            [
+                (
+                    np.where(self.index_label_correspondance == label)[0][0]
+                    if label in self.index_label_correspondance
+                    else 0
+                )
+                for label in range(max_label + 1)
+            ]
+        )
 
     @cached_property
     def _shifted_index_label_correspondance(self):
@@ -177,7 +206,7 @@ class Channel:
         list[int]
             the list L such that L[i]=index_label_correspondance[i]-1
         """
-        return list(np.array(self.index_label_correspondance) - 1)
+        return np.array(self.index_label_correspondance) - 1
 
     def nucleus(self, index):
         """Return the nucleus associated to a given index, as a mask.
@@ -280,7 +309,85 @@ class Channel:
 
         return self.masked_sizes(other_channel.binary_img) / self.sizes
 
-    def single_blob_overlap_rates(
+    def _set_blobwise_overlaps(self, other_channel: Channel):
+        """
+        Compute and store the matrix M of overlaps wher M[i,j] is the overlap between blob
+        of index i for self and the blob of index j for other_channel.
+        """
+        counts = bilabel_bincount(self.labels, other_channel.labels)
+        # we reorder the lines and cols from label orders to index orders
+        col_selector = np.zeros(
+            (counts.shape[1], len(other_channel.index_label_correspondance))
+        )
+        col_selector[
+            other_channel.index_label_correspondance, np.arange(col_selector.shape[1])
+        ] = 1
+
+        line_selector = np.zeros(
+            (len(self.index_label_correspondance), counts.shape[0])
+        )
+        line_selector[
+            np.arange(line_selector.shape[0]), self.index_label_correspondance
+        ] = 1
+
+        self._overlaps_dic[other_channel.name] = line_selector @ counts @ col_selector
+
+    def blobwise_overlaps(self, other_channel: Channel) -> np.ndarray:
+        """Return the matrix of overlaps.
+
+        Parameters
+        ----------
+        other_channel : Channel
+            The channel to compare with self.
+
+        Returns
+        -------
+        np.ndarray
+            The matrix M such that M[i,j] is the overlap count in voxels for blob
+            of index i for self and the blob of index j for other_channel.
+        """
+
+        if other_channel.name in self._overlaps_dic:
+            return self._overlaps_dic[other_channel.name]
+
+        elif self.name in other_channel._overlaps_dic:
+            return other_channel._overlaps_dic[self.name].transpose()
+        else:
+            self._set_blobwise_overlaps(other_channel)
+            return self._overlaps_dic[other_channel.name]
+
+    def max_blobwise_overlaps(
+        self, other_channel: Channel, return_max_indices=True
+    ) -> np.ndarray:
+        """For each nucleus of self, compute the max overlap with a single nucleus of other_channel.
+            if return_max_indices is True, nuclei indices that realize the max are also returned.
+
+        Parameters
+        ----------
+        other_channel : Channel
+            channel to compare with self
+
+        Returns
+        -------
+        np.ndarray
+            the max overlap (in voxels) for each nucleus in the order of self.dataframe
+
+        np.ndarray
+            the array of nucleus indices of other_channel that realize the maxima.
+        """
+
+        counts = self.blobwise_overlaps(other_channel)
+
+        if return_max_indices:
+            argmax = np.argmax(counts, axis=1)
+            maxima = counts[np.arange(counts.shape[0]), argmax]
+
+            return maxima, argmax
+
+        else:
+            return np.max(counts, axis=1)
+
+    def max_blobwise_overlap_rates(
         self, other_channel: Channel, return_max_indices=True
     ) -> np.ndarray:
         """For each nucleus of self, compute the max overlap rate with a single nucleus of other_channel.
@@ -300,36 +407,14 @@ class Channel:
             the array of nucleus indices of other_channel that realize the maxima.
         """
 
-        counts = bilabel_bincount(self.labels, other_channel.labels)
-
         if return_max_indices:
-            argmax = np.argmax(counts[1:, 1:], axis=1)
-            self_labels_to_other_channel_labels_of_max = argmax + 1
-            self_nuclei_indices_to_other_channel_labels_of_max = (
-                self_labels_to_other_channel_labels_of_max[
-                    self._shifted_index_label_correspondance
-                ]
+            maxima, argmax = self.max_blobwise_overlaps(
+                other_channel, return_max_indices=True
             )
-
-            self_nuclei_indices_to_other_channel_index_of_max = (
-                other_channel.label_index_correspondance[
-                    self_nuclei_indices_to_other_channel_labels_of_max
-                ]
-            )
-
-            max_by_label = counts[1:, 1:][np.arange(counts.shape[0] - 1), argmax]
-            max_by_index = max_by_label[self._shifted_index_label_correspondance]
-
-            return (
-                max_by_index / self.sizes,
-                self_nuclei_indices_to_other_channel_index_of_max,
-            )
-
+            return maxima / self.sizes, argmax
         else:
-            return (
-                np.max(counts[1:, 1:], axis=1)[self._shifted_index_label_correspondance]
-                / self.sizes
-            )
+            maxima = self.max_blobwise_overlaps(other_channel, return_max_indices=False)
+            return maxima / self.sizes
 
     def centers_distances(self, other_channel: Channel) -> np.array:
         """Return array of distances between centers across the two channels.
