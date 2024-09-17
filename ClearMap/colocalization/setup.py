1f3a5a99eda04d43ffde9e7eddfb3f77314c5d7e
Gaël Cousin <gcousin333@gmail.com>
2024-09-17 12:23:33 +0200

    added cython compiled batch version of bounding box computation

---
+++ ClearMap/colocalization/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+
+def make_ext(modname, pyxfilename):
+    import numpy as np
+    from distutils.extension import Extension
+
+    ext = Extension(
+        modname,
+        sources=[pyxfilename],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3"],
+        language="c++",
+    )
+
+    return ext
+
+
+ext = make_ext("bounding_boxes", "bbox.pyx")
+
+setup(ext_modules=cythonize([ext]))
