daef01157d665641c3d2c2e28438f263cf54702a
crousseau <charly.rousseau@icm-institute.org>
2024-12-02 14:56:45 +0100

    BUGFIX: Fixes argument name error in call to io.initialize in Resampling.resample

--- ClearMap/Alignment/Resampling.py
+++ ClearMap/Alignment/Resampling.py
@@ -606,8 +606,8 @@ def resample(original, resampled=None,
     if new_path is not None:
         delete_files.append(new_path)
     for step, axes, shape in zip(range(n_steps), axes_order, shape_order):
-        if step == n_steps - 1 and orientation is None:
-            resampled_data = io.initialize(source=resampled, shape=resampled_shape, dtype=dtype, as_source=True)
+        if step == n_steps - 1 and orientation is None:  # Create final resampled file for last step
+            resampled_data = io.initialize(resampled, shape=resampled_shape, dtype=dtype, as_source=True)
         else:
             if method == 'shared':
                 resampled_data = io.sma.create(shape, dtype=dtype, order=order, as_source=True)
