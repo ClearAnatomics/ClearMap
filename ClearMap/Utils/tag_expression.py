6bc2daad0c810b6b33a4a807e4e4588b644bea25
crousseau <charly.rousseau@icm-institute.org>
2024-12-02 14:32:46 +0100

    IMPROVEMENT: improves tag_expression by   - splitting glob to glob_pattern and glob (expected functionality)   - Adds methods (char_index, tag_min, tag_max, tag_range)   - Adds support for pattern finder ? symbols in glob_pattern   - Adds support for pathlib   - General refactoring   - Adds docstrings

--- ClearMap/Utils/tag_expression.py
+++ ClearMap/Utils/tag_expression.py
@@ -13,11 +13,11 @@ An expression is a filename with any number of tags.
 A tag has the format <Name,Type,Width>. 
   * Name : str that specifies the tag name
   * Type : 'I' or 'S' for integer or string, optional and defaults to 'I'
-  * Width : int, if given indicates a fixed width  and missing digist or chars
+  * Width : int, if given indicates a fixed width and missing digits or chars
     are replaced by trailing zeros or spaces.
 
 The module also provides functions to infer the tag expressions from a list of
-file names via the function :func:`~ClearMap.Utils.TagExpression.detect`.
+file names via the function :func:`~ClearMap.Utils.tag_expression.detect`.
 
 Expressions can also be converted to glob or regular expressions.
 
@@ -27,12 +27,12 @@ Example
 An example tag expression would be 'file_<X,2>_<Y,3>.npy' and would for example
 match the filename 'file_01_013.npy'
 
->>> import ClearMap.Utils.TagExpression as te
+>>> import ClearMap.Utils.tag_expression as te
 >>> e = te.Expression('file_<X,2>_<Y,3>.npy')
 >>> e.tag_names()
 e.tag_names()
 
->>> e.glob()
+>>> e.glob_pattern()
 'file_[0-9][0-9]_[0-9][0-9].npy'
 
 >>> e.indices('file_01_013.npy')
@@ -56,8 +56,12 @@ __download__ = 'https://github.com/ClearAnatomics/ClearMap'
 
 
 import copy
+import glob
 
 import re
+from pathlib import Path
+
+import natsort
 
 TAG_START = '<'
 TAG_END = '>'
@@ -82,7 +86,26 @@ def default_tag_name(index=None):
     return tag
 
 
-class Tag(object):
+class Tag:
+    """
+    A tag is a simple placeholder for a string or number in a file name.
+
+    A tag has the format <Name,Type,Width>.
+    * Name : str that specifies the tag name
+    * Type : 'I' or 'S' for integer or string, optional and defaults to 'I'
+    * Width : int, if given indicates a fixed width and missing digits or chars
+    are replaced by trailing zeros or spaces.
+
+    Attributes
+    ----------
+    name: str
+        The name of the tag
+    ttype: str
+        The type of the tag, either 'I' for integer or 'S' for string.
+        defined from the constants TAG_INT and TAG_STR
+    width: int
+        The width of the tag, i.e. the number of digits or characters
+    """
     def __init__(self, tag=None, name=None, ttype=TAG_INT, width=None, reference=None, trange=None):
         if tag is not None:
             self.parse(tag)
@@ -100,6 +123,14 @@ class Tag(object):
             return default_tag_name(index=index)
 
     def dtype(self):
+        """
+        Return the data type (python) of the tag.
+
+        Returns
+        -------
+        type
+            The data type of the tag.
+        """
         return ttype_to_dtype(self.ttype)
 
     def tag(self):
@@ -115,7 +146,7 @@ class Tag(object):
         t += TAG_END
         return t
 
-    def glob(self):
+    def glob_pattern(self):
         e = ''
         if self.width is not None:
             if self.ttype == TAG_INT or self.ttype is None:
@@ -146,7 +177,7 @@ class Tag(object):
             else:  # self.ttype == TAG_STR:
                 e += '.'
             if self.width is not None:
-                e += '{' + str(self.width) + '}'
+                e += f'{{{self.width}}}'
             else:
                 e += '*?'
             e += ')'
@@ -155,6 +186,8 @@ class Tag(object):
     def string(self, value=None):
         if value is None:
             return self.tag()
+        elif value == '?':
+            return '?' * self.width
 
         if self.width is None:
             if self.ttype == TAG_INT or self.ttype is None:
@@ -194,6 +227,25 @@ class Tag(object):
             raise IndexError(f'Value {value} not in tag range {self.trange}!')
 
     def parse(self, tag):
+        """
+        Parse a tag from a string. The string should be in the format of the tag.
+        i.e. f'{TAG_START}{Name}{TAG_SEPARATOR}{Type}{TAG_SEPARATOR}{Width}{TAG_END}'
+        tag_type and tag_width are optional but the tag name is required.
+        If any of the optional values are not given then the tag pattern is shortened
+        accordingly.
+        If tag_type is not given it defaults to 'I' for integer. If tag_width is not
+        given the tag is considered to have variable width.
+        This function will initialize the tag with the parsed values.
+
+        Parameters
+        ----------
+        tag: str
+            The tag string to parse.
+
+        Returns
+        -------
+        None
+        """
         if len(tag) < len(TAG_START) + len(TAG_END):
             raise ValueError(f'The string {tag} is not a valid tag!')
         if not tag.startswith(TAG_START):
@@ -206,31 +258,30 @@ class Tag(object):
             self.__init__()
             return
 
+        tag_attributes = {
+            'name': None,
+            'ttype': None,
+            'width': None
+        }
+
         values = tag.split(TAG_SEPARATOR)
         if len(values) > 3:
-            raise ValueError(f'Found {len(values)} > {3} tag specifications!')
-
-        tag_attributes = {
-            'name': [],
-            'ttype': [],
-            'width': []
-        }
-        for i, v in enumerate(values):
+            raise ValueError(f'Found {len(values)} > 3 tag attributes !')
+        elif len(values) == 3:
+            tag_attributes['name'] = values[0]
+            tag_attributes['ttype'] = values[1]
+            tag_attributes['width'] = int(values[2])
+        elif len(values) == 2:
+            tag_attributes['name'] = values[0]
+            tmp = values[1]
             try:
-                if i > 0:  # don't expect a width specification in the first entry
-                    tag_attributes['width'].append(int(v))
+                tag_attributes['width'] = int(tmp)
             except ValueError:
-                if v in [TAG_INT, TAG_STR]:
-                    tag_attributes['ttype'].append(v)
-                elif not v:
-                    raise ValueError('Found two tag separators without a value in between!')
-                else:
-                    tag_attributes['name'].append(v)
+                tag_attributes['ttype'] = tmp
 
-        for key, value in tag_attributes.items():
-            if len(value) > 1:
-                raise ValueError(f'More than one {key} found in tag: {value}')
-            tag_attributes[key] = value[0] if len(value) == 1 else None
+        for k, v in tag_attributes.items():
+            if v is not None and not v:
+                raise ValueError(f'Empty value for tag attribute {k} in {tag}! This is not allowed!')
 
         self.__init__(**tag_attributes)
 
@@ -241,20 +292,21 @@ class Tag(object):
         return self.__str__()
 
 
-class Expression(object):
+class Expression:
+    tag_regexp = re.compile(f'{TAG_START}.*?{TAG_END}')
     def __init__(self, pattern=None):
         if isinstance(pattern, Expression):
             self.pattern = copy.copy(pattern.pattern)
             self.tags = copy.copy(pattern.tags)
-        elif isinstance(pattern, str):
-            self.parse(pattern)
+        elif isinstance(pattern, (str, Path)):
+            self.parse(str(pattern))
         else:
             if pattern is None:
-                pattern = []
+                pattern = []  # FIXME: no plural in pattern what list is it? it seems tags
             self.pattern = pattern
             self.tags = [p for p in pattern if isinstance(p, Tag) and not p.reference]
 
-    def tag(self):
+    def tag(self):  # FIXME: confusing name. Does this "tag" the expression?
         e = ''
         for p in self.pattern:
             if isinstance(p, Tag):
@@ -263,7 +315,70 @@ class Expression(object):
                 e += p
         return e
 
+    def tag_max(self, tag_name):
+        """
+        Return the maximum value (i.e. range[-1]) of a tag in the expression.
+
+        Parameters
+        ----------
+        tag_name: str
+            The name of the tag
+
+        Returns
+        -------
+        int
+            The maximum value of the tag
+        """
+        return self.tag_range(tag_name)[1]
+
+    def tag_min(self, tag_name):
+        """
+        Return the minimum value (i.e. range[0]) of a tag in the expression.
+
+        Parameters
+        ----------
+        tag_name: str
+            The name of the tag
+
+        Returns
+        -------
+        int
+            The minimum value of the tag
+        """
+        return self.tag_range(tag_name)[0]
+
+    def tag_range(self, tag_name):
+        """
+        Return the range of values of a tag in the expression.
+
+        Parameters
+        ----------
+        tag_name: str
+            The name of the tag
+
+        Returns
+        -------
+        tuple[int]
+            The minimum and maximum value of the tag
+        """
+        file_list = self.glob(sort=True)
+        values = [self.values(f)[tag_name] for f in file_list]
+        return min(values), max(values)
+
     def re(self):
+        """
+        Convert the expression to a regular expression string.
+
+        Examples
+        --------
+        >>> e.re()
+        'file_(?P<X>\\d{2})_(?P<Y>\\d{3})\\.npy'
+
+        Returns
+        -------
+        str
+            The regular expression string.
+        """
         e = ''
         n_tag = 0
         for p in self.pattern:
@@ -274,31 +389,86 @@ class Expression(object):
                 e += re.escape(p)
         return e
 
-    def glob(self, values=None):
+    def glob_pattern(self, values=None):
+        """
+        Convert the expression to a glob pattern string.
+        >>> exp = Expression('file_<X,2>_<Y,3>.npy')
+
+        >>> exp.glob_pattern()
+        'file_[0-9][0-9]_[0-9][0-9].npy'
+
+        Parameters
+        ----------
+        values: dict
+            A dictionary with values for the tags
+
+        Returns
+        -------
+        str
+            The glob pattern string
+        """
         e = ''
         n_tag = 0
         for p in self.pattern:
             if isinstance(p, Tag):
                 if values is None:
-                    e += p.glob()
+                    e += p.glob_pattern()
                 else:
                     lab = p.label(n_tag)
                     if lab in values.keys():
                         e += escape_glob(p.string(value=values[lab]))
                     else:
-                        e += p.glob()
+                        e += p.glob_pattern()
                 n_tag += 1
             else:
                 e += escape_glob(p)
         return e
 
+    def glob(self, sort=False):
+        """
+        Execute the glob pattern.
+
+        Parameters
+        ----------
+        sort: bool
+            If True, the paths are sorted using natsort.
+
+        Returns
+        -------
+        list[str]
+            A list of paths that match the glob pattern.
+        """
+        paths = glob.glob(self.glob_pattern())
+        if sort:
+            paths = natsort.natsorted(paths)
+        return paths
+
     def string(self, values=None):
+        """
+        Convert a value to a string according to the tag specification.
+
+        Examples
+        --------
+        >>> exp = Expression('file_<X,2>_<Y,3>.npy')
+        >>> exp.string({'X':1, 'Y':10})
+        'file_01_010.npy'
+
+        Parameters
+        ----------
+        values: dict
+            A dictionary with values for the tags. Each key is the tag name.
+
+        Returns
+        -------
+        str
+            The string representation of the value.
+        """
         e = ''
         n_tag = 0
         for p in self.pattern:
             if isinstance(p, Tag):
                 if values is not None:
-                    v = values.get(p.label(n_tag), None)
+                    v = values.get(p.label(n_tag), None)  # FIXME: do not replace if not in values (use kwarg)
                 else:
                     v = None
                 e += p.string(value=v)
@@ -307,7 +477,54 @@ class Expression(object):
                 e += p
         return e
 
+    def char_index(self, tag_name, with_markups=False):
+        """
+        Return the character index range of the tag in the expression
+
+        Parameters
+        ----------
+        tag_name: str
+            The name of the tag
+        with_markups: bool
+            If True, the markup characters (<,width,type>) are included in the index range
+
+        Returns
+        -------
+        tuple[int]
+            The character index range of the tag in the expression
+        """
+        n_tag = 0
+        start = 0  # FIXME: exclude markups
+        for p in self.pattern:
+            if isinstance(p, Tag):
+                if p.label(n_tag) == tag_name:
+                    if with_markups:
+                        return start, start + len(p.string())
+                    else:
+                        return start, start + p.width
+                else:
+                    if with_markups:
+                        start += len(p.string())
+                    else:
+                        start += p.width
+                    n_tag += 1
+            else:
+                start += len(p)
+
     def values(self, string):
+        """
+        Extract the values from a string according to the tag specification.
+
+        Parameters
+        ----------
+        string: str
+            The expression string to extract the values from.
+
+        Returns
+        -------
+        dict
+            A dictionary with the values for the tags. Keys are the tag names.
+        """
         tags = self.tags
         search = re.compile(self.re()).search
         match = search(string)
@@ -324,19 +541,57 @@ class Expression(object):
             return d
 
     def string_from_index(self, indices):
+        """
+        Convert a dictionary or list of tag indices to a string according to the tag specification.
+
+        Examples
+        --------
+        >>> exp = Expression('file_<X,2>_<Y,3>.npy')
+        >>> exp.string_from_index([1, 13])
+        'file_01_013.npy'
+
+        Parameters
+        ----------
+        indices: list[int] | dict
+            A list of indices for the tags. If a dictionary is given, the keys are the tag names.
+
+        Returns
+        -------
+
+        """
+        if isinstance(indices, int):
+            indices = [indices]
         if not isinstance(indices, dict):
-            indices = {t.label(i): indices[i] for i, t in enumerate(self.tags)}
-        e = ''
-        n_tag = 0
+            indices = {name: idx for idx, name in zip(indices, self.tag_names())}
+        out = ''
+        n_tags = 0
         for p in self.pattern:
             if isinstance(p, Tag):
-                e += p.string_from_index(index=indices[p.label(n_tag)])
-                n_tag += 1
+                out += p.string_from_index(index=indices[p.label(n_tags)])
+                n_tags += 1
             else:
-                e += p
-        return e
+                out += p
+        return out
 
     def indices(self, string):
+        """
+        Infer the indices from a string.
+
+        Examples
+        --------
+        >>> e.indices('file_01_013.npy')
+        [1, 13]
+
+        Parameters
+        ----------
+        string: str
+            The path string to infer the indices from
+
+        Returns
+        -------
+        list[int]
+            The indices of the tags
+        """
         tags = self.tags
         search = re.compile(self.re()).search
         match = search(string)
@@ -356,7 +611,7 @@ class Expression(object):
     def tag_names(self):
         return [t.label(i) for i, t in enumerate(self.tags)]
 
-    def ntags(self):
+    def n_tags(self):
         return len(self.tags)
 
     def __getitem__(self, i):
@@ -369,11 +624,10 @@ class Expression(object):
             raise IndexError(f'No tag with name "{i}"!')
 
     def parse(self, expression):
-        p = re.compile(f'{TAG_START}.*?{TAG_END}')
         pattern = []
         tags = []
         start = 0
-        for match in p.finditer(expression):
+        for match in Expression.tag_regexp.finditer(expression):
             if match.start() > start:
                 pattern.append(expression[start:match.start()])
             tag = Tag(tag=match.group())
@@ -474,10 +728,10 @@ class Expression(object):
         self.tags = [q for q in pattern if isinstance(q, Tag) and not q.reference]
 
     def __str__(self):
-        return f'TagExpression({self.tag()})'
+        return f'{self.tag()}'
 
     def __repr__(self):
-        return self.__str__()
+        return f'tag_expression({self})'
 
 
 def parse(expression):
@@ -526,11 +780,10 @@ def _test():
 
     # glob
     import ClearMap.Tests.Files as tf
-    import glob
 
     s = tf.io.join(tf.tif_sequence, 'sequence<I,4>.tif')
     t = te.parse(s)
-    f = glob.glob(t.glob())
+    f = t.glob()
 
     # detection
     te.detect(f, names=['X'])
