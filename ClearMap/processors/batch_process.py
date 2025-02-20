dce7ea07b05caa3f9673553bf73ff22f7c83a508
crousseau <charly.rousseau@icm-institute.org>
2025-02-20 16:53:17 +0100

    UPDATE: updates batch_process for v3

--- ClearMap/processors/batch_process.py
+++ ClearMap/processors/batch_process.py
@@ -22,7 +22,7 @@ try:
 except ImportError:
     graph_gt = False
 from ClearMap.processors.cell_map import CellDetector
-from ClearMap.processors.sample_preparation import PreProcessor, init_preprocessor
+from ClearMap.processors.sample_preparation import  SampleManager, init_sample_manager_and_processors
 from ClearMap.config.config_loader import get_configs, ConfigLoader
 from ClearMap.IO import IO as clearmap_io
 
@@ -34,34 +34,30 @@ __download__ = 'https://github.com/ClearAnatomics/ClearMap'
 
 
 def process_sample(configs, align=False, cells=False, vasc=False):
-    patch_pipeline_name(configs, cells, vasc)
+    res = init_sample_manager_and_processors(configs=configs)
+    keys = ['sample_manager', 'stitching_processor', 'registration_processor']
+    sample_manager, stitching_processor, registration_processor = [res[k] for k in keys]
 
-    pre_proc = PreProcessor()
-    pre_proc.setup(configs)
-    pre_proc.setup_atlases()
     if align:
-        pre_proc.run()
+        stitching_processor.stitch()
+        registration_processor.resample_for_registration()
+        registration_processor.align()
     if cells:
-        cell_detector = CellDetector(pre_proc)
-        cell_detector.processing_config.reload()
-
-        cell_detector.run_cell_detection()
-        cell_detector.post_process_cells()
-        cell_detector.voxelize()
+        for channel in sample_manager.config_loader.get_cfg('cell_map')['channels'].keys():
+            cell_detector = CellDetector(sample_manager, channel=channel, registration_processor=registration_processor)
+            cell_detector.processing_config.reload()
+            cell_detector.run_cell_detection()
+            cell_detector.post_process_cells()
+            cell_detector.voxelize()
     if vasc:
-        binary_vessel_processor = BinaryVesselProcessor(pre_proc)
+
+        binary_vessel_processor = BinaryVesselProcessor(sample_manager)
         binary_vessel_processor.binarize()
         binary_vessel_processor.combine_binary()
 
-        vessel_graph_processor = VesselGraphProcessor(pre_proc)
+        vessel_graph_processor = VesselGraphProcessor(sample_manager, registration_processor)
         vessel_graph_processor.pre_process()
         vessel_graph_processor.post_process()
-
-
-def patch_pipeline_name(configs, cells, vasc):
-    configs[2]['pipeline_name'] = 'CellMap' if cells else 'TubeMap'
-    if cells and vasc:
-        configs[2]['pipeline_name'] = 'Both'
 
 
 class BatchProcessor:
@@ -75,7 +71,7 @@ class BatchProcessor:
         paths = [p for ps in self.params.get_all_paths() for p in ps]  # flatten list
         for folder in tqdm(paths, desc='Processing sample ', unit='brain'):
             cfg_loader = ConfigLoader(folder)
-            configs = get_configs(cfg_loader.get_cfg_path('sample'), cfg_loader.get_cfg_path('processing'))
+            configs = get_configs(cfg_loader.get_cfg_path('sample'), cfg_loader.get_cfg_path('processing'))  # FIXME
             process_sample(configs,  align=self.params.align, cells=self.params.count_cells,
                            vasc=self.params.run_vaculature)
         self.progress_watcher.finish()
@@ -84,7 +80,7 @@ class BatchProcessor:
 def process_folders(folders, align=False, cells=False, vasc=False):
     for folder in tqdm(folders, desc='Processing sample ', unit='brain'):
         cfg_loader = ConfigLoader(folder)
-        configs = get_configs(cfg_loader.get_cfg_path('sample'), cfg_loader.get_cfg_path('processing'))
+        configs = get_configs(cfg_loader.get_cfg_path('sample'), cfg_loader.get_cfg_path('processing'))  # FIXME:
         process_sample(configs, align=align, cells=cells, vasc=vasc)
 
 
@@ -95,45 +91,54 @@ def main(samples_file):
     voxelize_folders(folders)
 
 
-def voxelize_sample(configs, align=False, cells=False, vasc=False):
-    patch_pipeline_name(configs, cells, vasc)
+def voxelize_sample(configs, align=False, cells=False, vasc=False, voxelization_radius=(10, 10, 10)):
+    res = init_sample_manager_and_processors(configs=configs)
+    keys = ['sample_manager', 'stitching_processor', 'registration_processor']
+    sample_manager, stitching_processor, registration_processor = [res[k] for k in keys]
 
-    pre_proc = PreProcessor()
-    pre_proc.setup(configs)
-    pre_proc.setup_atlases()
     if align:
-        pre_proc.run()
+        stitching_processor.stitch()
+        registration_processor.resample_for_registration()
+        registration_processor.align()
     if cells:
-        cell_detector = CellDetector(pre_proc)
-
-        cell_detector.processing_config.reload()
-        # cell_detector.atlas_align()
-        # cell_detector.export_collapsed_stats()
-        cell_detector.processing_config['voxelization']['radii'] = (10, 10, 10)
-        cell_detector.processing_config.write()
-        cell_detector.voxelize()
+        for channel in sample_manager.config_loader.get_cfg('cell_map')['channels'].keys():
+            cell_detector = CellDetector(sample_manager, channel=channel, registration_processor=registration_processor)
+            cell_detector.processing_config.reload()
+            # cell_detector.atlas_align()
+            # cell_detector.export_collapsed_stats()
+            cell_detector.processing_config['voxelization']['radii'] = voxelization_radius
+            cell_detector.processing_config.write()
+            cell_detector.voxelize()
 
 
 def voxelize_folders(folders, align=False, cells=True, vasc=False):
     for folder in tqdm(folders, desc='Processing sample ', unit='brain'):
         cfg_loader = ConfigLoader(folder)
-        configs = get_configs(cfg_loader.get_cfg_path('sample'), cfg_loader.get_cfg_path('processing'))
+        configs = get_configs(cfg_loader.get_cfg_path('sample'), cfg_loader.get_cfg_path('processing'))  # FIXME:
         voxelize_sample(configs, align=align, cells=cells, vasc=vasc)
 
 
 def convert_to_cm_2_1(folder, atlas_base_name='ABA_25um'):
-    pre_proc = init_preprocessor(folder, atlas_base_name)
-    pre_proc.setup_atlases()
-    cell_detector = CellDetector(pre_proc)
-    cell_detector.convert_cm2_to_cm2_1_fmt()
+    res = init_sample_manager_and_processors(folder)
+    sample_manager = res['sample_manager']
+    registration_processor = res['registration_processor']
+    cell_detection_config = sample_manager.config_loader.get_cfg('cell_map')
+
+    for channel in cell_detection_config['channels'].keys():
+        cell_detector = CellDetector(sample_manager, channel=channel, registration_processor=registration_processor)
+        cell_detector.convert_cm2_to_cm2_1_fmt()
 
 
 def realign(folder, atlas_base_name='ABA_25um'):
-    pre_proc = init_preprocessor(folder, atlas_base_name)
-    pre_proc.setup_atlases()
-    cell_detector = CellDetector(pre_proc)
-    cell_detector.filter_cells()
-    cell_detector.atlas_align()
+    res = init_sample_manager_and_processors(folder)
+    sample_manager = res['sample_manager']
+    registration_processor = res['registration_processor']
+    cell_detection_config = sample_manager.config_loader.get_cfg('cell_map')
+
+    for channel in cell_detection_config['channels'].keys():
+        cell_detector = CellDetector(sample_manager, channel=channel, registration_processor=registration_processor)
+        cell_detector.filter_cells()
+        cell_detector.atlas_align()
 
 
 def rescale_img(f_path, scaling_factor):
@@ -163,32 +168,37 @@ def rescale_channel(folder, atlas_base_name=None, dest_resolution=(3, 3, 6), n_c
     """
     n_cpus = multiprocessing.cpu_count() - 2 if n_cpus is None else n_cpus
 
-    pre_proc = init_preprocessor(folder, atlas_base_name)
-    file_list = pre_proc.workspace.file_list(channel, prefix=pre_proc.prefix, extension=ext)
+    res = init_sample_manager_and_processors(folder)
+    sample_manager = res['sample_manager']
+
+    file_list = sample_manager.get('raw', channel=channel).variant(extension=ext).file_list
     print(f'Processing {file_list}')
 
-    scaling_factors = np.array(pre_proc.sample_config['resolutions'][channel]) / np.array(dest_resolution)
+    scaling_factors = np.array(sample_manager.config['channels'][channel]['resolution']) / np.array(dest_resolution)
     print(scaling_factors)
     rescale_f = functools.partial(rescale_img, scaling_factor=tuple(scaling_factors))
 
-    if n_cpus*chunk_size == 1:
+    if n_cpus * chunk_size == 1:
         for f_path in file_list:
             rescale_f(f_path)
     else:
         with ThreadPoolExecutor(n_cpus) as executor:
             executor.map(rescale_f, file_list, chunksize=chunk_size)
 
-    pre_proc.sample_config['resolutions'][channel] = list(dest_resolution)
-    pre_proc.sample_config.write()
+    sample_manager.config['channels'][channel]['resolution'] = list(dest_resolution)
+    sample_manager.config.write()
     print('DONE')
 
 
-def batch_crop(folder, crop_x=0, crop_y=0, suffix='_cropped'):
-    from ClearMap.processors.sample_preparation import init_preprocessor
-    preproc = init_preprocessor(folder)
+def batch_crop(folder, crop_x=0, crop_y=0, suffix='_cropped', channel=None):
+    if channel is None:
+        raise ValueError('Please provide a channel')
+    res = init_sample_manager_and_processors(folder)
+    sample_manager = res['sample_manager']
 
-    shape = clearmap_io.shape(preproc.workspace.file_list('cfos')[-1])
-    for f_path in preproc.workspace.file_list('cfos'):
+    raw_asset = sample_manager.get('raw', channel=channel)
+    shape = clearmap_io.shape(raw_asset.filelist[-1])
+    for f_path in raw_asset.filelist:
         img = clearmap_io.read(f_path)
         cropped_img = img[crop_x:shape[0] - crop_x, crop_y:shape[1] - crop_y, :]
         cropped_path = f_path.replace('.ome.tif', f'{suffix}.ome.tif')
