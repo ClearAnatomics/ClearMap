fd1aa01ec1144bcd3f725d3f5adf11a274b43091
crousseau <charly.rousseau@icm-institute.org>
2025-02-20 16:51:39 +0100

    UPDATE: updates align_new_api to V3

--- ClearMap/Scripts/align_new_api.py
+++ ClearMap/Scripts/align_new_api.py
@@ -4,49 +4,53 @@ import os
 from ClearMap.IO import IO as clearmap_io
 from ClearMap.IO.MHD import read as mhd_read
 from ClearMap.Visualization.Qt import Plot3d as q_plot_3d
+from ClearMap.Visualization.Qt.utils import link_dataviewers_cursors
+from ClearMap.processors.sample_preparation import StitchingProcessor, RegistrationProcessor
 
 
-def plot_registration_results(pre_proc):
-    img_paths = (pre_proc.filename('resampled', postfix='autofluorescence'),
-                 pre_proc.filename('aligned_to_reference', channel=pre_proc.sample_manager.alignment_reference_channel))
-    image_sources = copy.deepcopy(list(img_paths))
-    for i, im_path in enumerate(image_sources):
-        if im_path.endswith('.mhd'):
-            image_sources[i] = mhd_read(im_path)
-    titles = [os.path.basename(img) for img in img_paths]
-    q_plot_3d.plot([image_sources, ], title=' '.join(titles), arrange=True, sync=True, lut='white')
+def plot_registration_results(aligner: RegistrationProcessor, channel: str, composite: bool = False):
+    img_paths = [
+        aligner.get_fixed_image(channel).path,
+        aligner.get_aligned_image(channel),
+    ]
+    if not all([p.exists() for p in img_paths]):
+        raise ValueError(f'Missing requirements {img_paths}')
+    # image_sources = copy.deepcopy(list(img_paths))
+    # for i, im_path in enumerate(image_sources):
+    #     if im_path.endswith('.mhd'):
+    #         image_sources[i] = mhd_read(im_path)
+    titles = [img.parent.stem if 'aligned_to' in str(img) else img.stem for img in img_paths]
+    if composite:
+        img_paths = [img_paths, ]
+    dvs = q_plot_3d.plot(img_paths, title=titles, arrange=True, sync=True, lut='white')
+    if not composite:
+        link_dataviewers_cursors(dvs)
 
 
-def register(pre_proc):
+def register(aligner: RegistrationProcessor):
     print('Registering')
-    pre_proc.setup_atlases()
-    for channel in pre_proc.channels_to_resample:
-        pre_proc.resample_channel(channel)
+    aligner.setup_atlases()
+    for channel in aligner.channels_to_resample():
+        aligner.resample_channel(channel)
     print('Aligning')
-    pre_proc.align()
+    aligner.align()
     print('Registered')
 
 
-def convert_stitched(pre_proc):  # FIXME: removed
-    if not pre_proc.stitching_cfg['output_conversion']['skip']:
-        fmt = pre_proc.stitching_cfg['output_conversion']['format']
-        print(f'Converting stitched image to {fmt}')
-        pre_proc.convert_to_image_format()
+def stitch(stitcher: StitchingProcessor):  # FIXME: part of stitcher object
+    sample_manager = stitcher.sample_manager
+    for channel in sample_manager.channels:
+        if not sample_manager.is_tiled(channel):
+            stitcher.copy_or_stack(channel)
 
-
-def stitch(pre_proc):
-    tags = pre_proc.workspace.expression('raw', prefix=pre_proc.prefix).tags
-    if tags is not None:
-        axes = [tag.name for tag in tags]
-    if tags is None or axes == ['Z']:  # BYPASS stitching, just copy or stack
-        clearmap_io.convert(pre_proc.filename('raw'), pre_proc.filename('stitched'))
-    else:  # assume tiling
-        if not pre_proc.stitching_cfg['rigid']['skip']:
-            pre_proc.stitch_rigid(force=True)
-            print('Stitched rigid')
-        if not pre_proc.stitching_cfg['wobbly']['skip']:
-            if pre_proc.channel_was_stitched_rigid(channel):
-                pre_proc.stitch_wobbly(force=pre_proc.stitching_cfg['rigid']['skip'])
-                print('Stitched wobbly')
-            else:
-                print('Could not run wobbly stitching <br>without rigid stitching first')
+    for channel in stitcher.get_stitching_order():
+        config = stitcher.config['channels'][channel]
+        if not config['run']:
+            continue
+        if config['use_npy'] and not sample_manager.has_npy(channel):
+                stitcher.convert_tiles()
+        if channel == config['layout_channel']:
+            stitcher.stitch_channel_rigid(channel, _force=True)
+            stitcher.stitch_channel_wobbly(channel, _force=True)  # TODO: check if force
+        else:
+            stitcher._stitch_layout_wobbly(channel)
