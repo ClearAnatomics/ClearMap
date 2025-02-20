03e6b7d8170c09db8756863c56c677221394a676
crousseau <charly.rousseau@icm-institute.org>
2025-02-20 17:04:18 +0100

    UPDATE: Updates Stitching.plot_utils for v3

--- ClearMap/Alignment/Stitching/plot_utils.py
+++ ClearMap/Alignment/Stitching/plot_utils.py
@@ -5,25 +5,18 @@ from matplotlib import pyplot as plt
 
 sys.path.insert(0, os.path.abspath('.'))
 
-from ClearMap.IO.metadata import define_auto_stitching_params
-from ClearMap.processors.sample_preparation import init_preprocessor
+from ClearMap.processors.sample_preparation import SampleManager, StitchingProcessor
 import ClearMap.Alignment.Stitching.StitchingRigid as stitching_rigid
 
 
-def stitch_and_plot_layout(channel):
-    pre_proc = init_preprocessor('/data/test/')
-    stitching_cfg = pre_proc.processing_config['stitching'][channel]['rigid']
-    overlaps, projection_thickness = define_auto_stitching_params(
-        pre_proc.workspace.source('raw', channel=channel).file_list[0], stitching_cfg)
-    layout = pre_proc.get_wobbly_layout(overlaps)
-    return pre_proc.overlay_layout_plane(layout.copy())
-
 
 def plot_all_layouts(folder):
-    pre_proc = init_preprocessor(folder)
+    sample_manager = SampleManager()
+    sample_manager.setup(folder)
+    stitcher = StitchingProcessor(sample_manager)
     for postfix in ('aligned_axis', 'aligned', 'placed'):
-        layout = stitching_rigid.load_layout(pre_proc.filename('layout', postfix=postfix))
-        overlay = pre_proc.overlay_layout_plane(layout)
+        layout = stitching_rigid.load_layout(sample_manager.get_path('layout', postfix=postfix))
+        overlay = stitcher.overlay_layout_plane(layout)
         plt.imshow(overlay)
         plt.show()
 
