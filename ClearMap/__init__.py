de35ecc2ac7c20da0259e655163fb6bf1369383b
Charly Rousseau <crousseau@users.noreply.github.com>
2025-02-26 10:44:34 +0100

    Update __init__.py

--- ClearMap/__init__.py
+++ ClearMap/__init__.py
@@ -43,5 +43,4 @@ __download__ = 'https://github.com/ClearAnatomics/ClearMap'
 try:
     __version__ = version("ClearMap")
 except PackageNotFoundError:
-    __version__ = "2.1.0"
-from . import colocalization
+    __version__ = '3.0.0'
