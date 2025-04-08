ClearAnatomics/ClearMap

commit 981f4823155585481d899954eaf5940d34074b78

REFACTORING: Cleans method reload_config of tract_map and colocalization

ClearMap/processors/colocalization.py
@@ -90,13 +90,12 @@ class ColocalizationProcessor(TabProcessor):
     #   make sure this works with other config backends.
     def reload_config(self, max_iter=10):
         p = self.processing_config
-        iter = 0
-        while not hasattr(p, 'reload'):
-            p = p.parent
-            iter += 1
-            if iter > max_iter:
-                raise ValueError('Could not find a reload method in the config')
-        p.reload()
+        for i in range(max_iter):
+            if hasattr(p, 'reload'):
+                p.reload()
+                return
+        else:
+            raise ValueError(f'Could not find a reload method in the config after {max_iter} iterations')
 
     def get_cells_df(self, channel):
         return pd.read_feather(self.get_path('cells', channel=channel))

ClearMap/processors/tract_map.py
@@ -69,13 +69,12 @@ class TractMapProcessor(TabProcessor):
     #   make sure this works with other config backends.
     def reload_config(self, max_iter=10):
         p = self.processing_config
-        iter = 0
-        while not hasattr(p, 'reload'):
-            p = p.parent
-            iter += 1
-            if iter > max_iter:
-                raise ValueError('Could not find a reload method in the config')
-        p.reload()
+        for i in range(max_iter):
+            if hasattr(p, 'reload'):
+                p.reload()
+                return
+        else:
+            raise ValueError(f'Could not find a reload method in the config, after {max_iter} iterations')
 
     def create_test_dataset(self, slicing):
         self.workspace.create_debug('stitched', channel=self.channel, slicing=slicing)

commit 251f72c1549747c8c5357a121093492a5812df46

BUGFIX: Fixes bug introduced by 981f4823155585481d

ClearMap/processors/cell_map.py
@@ -101,13 +101,14 @@ class CellDetector(TabProcessor):
     #   make sure this works with other config backends.
     def reload_config(self, max_iter=10):
         p = self.processing_config
-        iter = 0
-        while not hasattr(p, 'reload'):
-            p = p.parent
-            iter += 1
-            if iter > max_iter:
-                raise ValueError('Could not find a reload method in the config')
-        p.reload()
+        for i in range(max_iter):
+            if hasattr(p, 'reload'):
+                p.reload()
+                return
+            else:
+                p = p.parent
+        else:
+            raise ValueError(f'Could not find a reload method in the config, after {max_iter} iterations')
 
     def post_process_cells(self):
         self.reload_config()

ClearMap/processors/colocalization.py
@@ -94,6 +94,8 @@ class ColocalizationProcessor(TabProcessor):
             if hasattr(p, 'reload'):
                 p.reload()
                 return
+            else:
+                p = p.parent
         else:
             raise ValueError(f'Could not find a reload method in the config after {max_iter} iterations')
 

ClearMap/processors/tract_map.py
@@ -73,6 +73,8 @@ class TractMapProcessor(TabProcessor):
             if hasattr(p, 'reload'):
                 p.reload()
                 return
+            else:
+                p = p.parent
         else:
             raise ValueError(f'Could not find a reload method in the config, after {max_iter} iterations')
 