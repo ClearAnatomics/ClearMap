BUGFIX: Fixes markers scaling issue in CellCounter tab and Colocalization tab when zooming

ClearMap/Visualization/Qt/DataViewer.py
@@ -134,6 +134,7 @@ class DataViewer(QWidget):
         self.view = pg.ViewBox()
         self.view.setAspectLocked(True)
         self.view.invertY(invertY)
+        self.view.sigRangeChanged.connect(self.onRangeChanged)
 
         self.graphicsView = pg.GraphicsView()
         self.graphicsView.setObjectName("GraphicsView")
@@ -582,6 +583,10 @@ class DataViewer(QWidget):
         """
         self.sliceLine.setValue(self.sliceLine.value() + 1)
         self.sliceLine.setValue(self.sliceLine.value() - 1)
+        # if self.scatter is not None:
+        #     ax = self.scroll_axis
+        #     index = min(max(0, int(self.sliceLine.value())), self.source_shape[ax] - 1)
+        #     self.plot_scatter_markers(ax, index)
 
     def setSliceAxis(self, axis):
         # old_scroll_axis = self.scroll_axis
@@ -630,21 +635,27 @@ class DataViewer(QWidget):
         for s, mM in enumerate(min_max):
             self.luts[s].lut.region.setRegion(mM)
 
+    def onRangeChanged(self):
+        if self.scatter is not None:
+            ax = self.scroll_axis
+            index = min(max(0, int(self.sliceLine.value())), self.source_shape[ax] - 1)
+            self.plot_scatter_markers(ax, index)
+
+
     def plot_scatter_markers(self, ax, index):
         self.scatter.clear()
         self.scatter_coords.axis = ax
         pos = self.scatter_coords.get_pos(index)
         x_range, y_range = self.view.viewRange()
         # Compute scale from the ratio between original and current view range
-        # TODO: link to     self.view.sigRangeChanged.connect(self.onRangeChanged) so it updates on zoom too
+
         scale_x = self.source_range_x / (x_range[1] - x_range[0])
         scale_y = self.source_range_y / (y_range[1] - y_range[0])
         # transform = self.view.viewTransform()
         # scale_x, scale_y = transform.m11(), transform.m22()
         zoom_factor = (scale_x + scale_y) / 2.0
 
-        base_size = 10.0  # FIXME: that should be part of self.scatter
-        scaled_size = base_size * zoom_factor
+        scaled_size = round(self.scatter_coords.marker_size * zoom_factor)
 
         if all(pos.shape):
             if self.scatter_coords.has_colours:
@@ -811,9 +822,9 @@ class DataViewer(QWidget):
 
             px, py = np.zeros(x.shape[0] * 2), np.zeros(y.shape[0] * 2)
             l = 0.45
-            px[0::2] = x - l * vx;
+            px[0::2] = x - l * vx
             px[1::2] = x + l * vx
-            py[0::2] = y - l * vy;
+            py[0::2] = y - l * vy
             py[1::2] = y + l * vy
             path = pg.arrayToQPath(px, py, 'pairs')
 

ClearMap/processors/cell_map.py
@@ -415,8 +415,11 @@ class CellDetector(TabProcessor):
         color_map = {id_: annotator.convert_label(id_, key='id', value='color_hex_triplet') for id_ in unique_ids}
         color_map[0] = np.array(to_hex((1, 0, 0)))  # default to red
         df['color'] = df['id'].map(color_map)
+
+        particle_size = self.processing_config['detection']['background_correction']['diameter'][0]
         dv.scatter_coords = Scatter3D(coordinates, colors=df['color'].to_list(),
-                                      hemispheres=hemispheres, half_slice_thickness=0)
+                                      hemispheres=hemispheres, half_slice_thickness=0,
+                                      marker_size=particle_size // 5)
         dv.refresh()
         return [dv]
 