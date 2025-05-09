1420631fced2023feb45274c0c33c9d1bde52313
Etienne Doumazane <etienne.doumazane@gmail.com>
2025-05-09 13:07:32 +0200

    Refactoring

--- ClearMap/Scripts/custom_cells.py
+++ ClearMap/Scripts/custom_cells.py
@@ -1,35 +1,40 @@
-from ClearMap.ImageProcessing.Experts.Cells import default_cell_detection_parameter, detect_cells
-from skimage.filters import threshold_otsu
-import numpy as np
+# TODO module-level documentation: This module is an example of a custom pipeline
+"""
+Example usage:
+```
+from ClearMap.Scripts.custom_cells import detect_cells, custom_detect_cells_block, default_cell_detection_parameter
+from skimage.data import cells3d
 
-from skimage.morphology import remove_small_objects,  remove_small_holes, binary_closing, ball, binary_opening
-from skimage.measure import label
-from skimage.measure import regionprops
+test_data = cells3d()[:,1]
+sink = detect_cells(test_data, block_detection_function=custom_detect_cells_block, cell_detection_parameter=cell_detection_parameter)
+```
+"""
 
-
-from ClearMap.ImageProcessing.Experts.Cells import detect_cells, default_cell_detection_parameter
-
-import warnings
-import numpy as np
-import ClearMap.Utils.Timer as tmr
-
-import scipy.ndimage.filters as ndf
-import scipy.ndimage as ndi
-
-from ClearMap.Utils.exceptions import ClearMapValueError
-from ClearMap.ImageProcessing.Experts.utils import run_step, print_params
-from ClearMap.ImageProcessing.Experts.Cells import remove_background, equalize, dog_filter
 import gc
-import ClearMap.Analysis.Measurements.shape_detection as sd
-import ClearMap.Analysis.Measurements.MeasureExpression as me
 
-import ClearMap.ImageProcessing.IlluminationCorrection as ic
-import ClearMap.ImageProcessing.Filter.StructureElement as se
-import ClearMap.ImageProcessing.Filter.FilterKernel as fk
+import numpy as np
 
-import ClearMap.Analysis.Measurements.maxima_detection as md
-import ClearMap.Analysis.Measurements.shape_detection as sd
-import ClearMap.Analysis.Measurements.MeasureExpression as me
+from skimage.filters import threshold_otsu
+from skimage.morphology import (
+    remove_small_objects,
+    remove_small_holes,
+    binary_closing,
+    ball,
+)
+from skimage.measure import label, regionprops
+
+from ClearMap.ImageProcessing.Experts.Cells import (
+    default_cell_detection_parameter,
+    detect_cells,
+)  # We need `detect_cells` imported from the same module as `custom_detect_cells_block`
+import ClearMap.Utils.Timer as tmr
+from ClearMap.ImageProcessing.Experts.utils import run_step
+
+
+default_cell_detection_parameter["custom_step"] = {
+    "save": False
+}  # The `cell_detection_parameter` dictionnary needs a key with the name of the new custom step
+
 
 def custom_detection(array, **kwargs):
     """Detect cells in a 3D array.
@@ -37,7 +42,7 @@ def custom_detection(array, **kwargs):
     and properties of each detected cell."""
 
     ## detect intensities above otsu threshold
-    mask = (array > threshold_otsu(array))
+    mask = array > threshold_otsu(array)
 
     ## postprocess mask
     mask = remove_small_objects(mask, min_size=1000)
@@ -56,6 +61,7 @@ def custom_detection(array, **kwargs):
 
     return coordinates, intensities, maxima, shapes
 
+
 def custom_detect_cells_block(source, parameter=default_cell_detection_parameter, n_threads=None):
     """Detect cells in a block of a 3D image.
     Outputs a tuple of arrays, representing the coordinates (x, y, z) of the cells **in the image**,
@@ -63,11 +69,11 @@ def custom_detect_cells_block(source, parameter=default_cell_detection_parameter
     That function can be passed to CellMap's `detect_cells` function."""
 
     # initialize parameter and slicing
-    if parameter.get('verbose'):
-        prefix = 'Block %s: ' % (source.info(),)
+    if parameter.get("verbose"):
+        prefix = "Block %s: " % (source.info(),)
         total_time = tmr.Timer(prefix)
     else:
-        prefix = ''
+        prefix = ""
 
     base_slicing = source.valid.base_slicing
     valid_slicing = source.valid.slicing
@@ -75,47 +81,58 @@ def custom_detect_cells_block(source, parameter=default_cell_detection_parameter
     valid_upper = source.valid.upper
     lower = source.lower
 
-
     # Measurements that will be performed per cell
     steps_to_measure = {}  # FIXME: rename
 
     ## intensity
-    parameter_intensity = parameter.get('intensity_detection')
+    parameter_intensity = parameter.get("intensity_detection")
     if parameter_intensity:
         parameter_intensity = parameter_intensity.copy()
-        measure = parameter_intensity.pop('measure', [])
+        measure = parameter_intensity.pop("measure", [])
         measure = measure if measure else []
         ## validation
-        valid_measurement_keys = list(default_cell_detection_parameter.keys()) + ['source']
+        valid_measurement_keys = list(default_cell_detection_parameter.keys()) + [
+            "source"
+        ]
         for m in measure:
             if m not in valid_measurement_keys:
-                raise KeyError(f'Unknown measurement: {m}')
+                raise KeyError(f"Unknown measurement: {m}")
             steps_to_measure[m] = None
     ## in case source is measured, the image used is source
-    if 'source' in steps_to_measure:
-        steps_to_measure['source'] = np.array(source.array)
+    if "source" in steps_to_measure:
+        steps_to_measure["source"] = np.array(source.array)
     ## other cases seem to not be supported
 
-    step_params = {'parameter': parameter, 'steps_to_measure': steps_to_measure, 'prefix': prefix,
-                           'base_slicing': base_slicing, 'valid_slicing': valid_slicing}
+    step_params = {
+        "parameter": parameter,
+        "steps_to_measure": steps_to_measure,
+        "prefix": prefix,
+        "base_slicing": base_slicing,
+        "valid_slicing": valid_slicing,
+    }
 
     # WARNING: if param_illumination: previous_step = source, not np.array(source.array)
 
-    results = run_step("custom_detection", np.array(source.array),
-                       custom_detection, **step_params)
+    results = run_step(
+        "custom_step", np.array(source.array), custom_detection, **step_params
+    )
 
     # correct coordinate offsets of blocks
     results = (results[0] + lower,) + results[1:]
 
     # remove cells outside the valid region of the block
-    valid_mask = np.all((results[0] >= valid_lower ) & (results[0] < valid_upper), axis=1)
+    valid_mask = np.all(
+        (results[0] >= valid_lower) & (results[0] < valid_upper), axis=1
+    )
     results = tuple(result[valid_mask] for result in results)
 
     # ensure all results array have 2 dimensions, so that they are ready to be vstacked
-    results = tuple(result[:, None] if result.ndim == 1 else result for result in results)
+    results = tuple(
+        result[:, None] if result.ndim == 1 else result for result in results
+    )
 
-    if parameter.get('verbose'):
-        total_time.print_elapsed_time('Cell detection')
+    if parameter.get("verbose"):
+        total_time.print_elapsed_time("Cell detection")
 
     gc.collect()
     return results
