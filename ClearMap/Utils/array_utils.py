c42c7caea5588c5378344dd5c13006c3c0e09dfb
crousseau <charly.rousseau@icm-institute.org>
2025-07-17 15:29:48 +0200

    IMPROVEMENT: Adds sanity checks to array_utils.remap_array_ranges

--- ClearMap/Utils/array_utils.py
+++ ClearMap/Utils/array_utils.py
@@ -7,6 +7,7 @@ It takes as input the source array, the new array, the source ranges, and the ne
 returns the remapped array.
 """
 import os
+import warnings
 
 import numpy as np
 from pyximport import pyximport
@@ -18,16 +19,43 @@ from ClearMap.Utils.array_utils_cy import remap_array_ranges_1_d, remap_array_ra
 
 
 def remap_array_ranges(src_array, new_array, src_ranges, new_ranges):
+    if not np.all(src_ranges[:, 1] >= src_ranges[:, 0]):
+        raise ValueError("src_ranges not sorted, please flip edges if necessary")
+    if not np.all(new_ranges[:, 1] >= new_ranges[:, 0]):
+        raise ValueError("new_ranges not sorted, please flip edges if necessary")
+
+    if src_ranges.max() > src_array.shape[0]:
+        raise ValueError(f'src_ranges {src_ranges.max()} is larger than the source array shape {src_array.shape[0]}')
+    if new_ranges.max() > new_array.shape[0]:
+        raise ValueError(f'new_ranges {new_ranges.max()} is larger than the new array shape {new_array.shape[0]}')
+
+    src_len = np.diff(src_ranges, axis=1).ravel()
+    dst_len = np.diff(new_ranges, axis=1).ravel()
+
+    assert src_len.shape == dst_len.shape, "Source and destination ranges do not have the same length."
+
     functions_map = {
         1: remap_array_ranges_1_d,
         2: remap_array_ranges_2_d
     }
     if src_array.ndim not in functions_map.keys():
-        raise ValueError(f"Only arrays with dimensions {' or '.join([str(k) for k in functions_map.keys()])}"
-                         f" are supported")
+        raise ValueError(f'remap_array_ranges only supports arrays with dimensions in {list(functions_map.keys())}')
     else:
-        result = functions_map[src_array.ndim](src_array, new_array, src_ranges, new_ranges)
+        f = functions_map[src_array.ndim]
+        result = f(src_array, new_array, src_ranges, new_ranges)
         return np.asarray(result, dtype=src_array.dtype)
+
+    # warnings.warn(f'Source and destination ranges do not have the same length, defaulting to np.take.')
+    #
+    # new_n_rows = int(new_ranges[-1, 1])
+    #
+    # out_shape = (new_n_rows,) + src_array.shape[1:]
+    # out = np.zeros(out_shape, dtype=src_array.dtype)
+    #
+    # for (src_start, src_end), (new_start, new_end) in zip(src_ranges, new_ranges):
+    #     out[new_start:new_end] = src_array[src_start:src_end]
+    #
+    # return out
 
 
 def dtype_range(arr):
