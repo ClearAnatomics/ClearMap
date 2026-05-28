674d800884edb9a01813ddcb0f517ce3aeb65197
crousseau <charly.rousseau@icm-institute.org>
2026-05-28 12:41:28 +0200

    IMPROVEMENT: Adds lazy_mixin decorator so we can inject plotting methods to e.g. orchestrators with lazy graphical dependencies

--- ClearMap/Utils/Lazy.py
+++ ClearMap/Utils/Lazy.py
@@ -8,8 +8,38 @@ Lazy evaluation attributes.
 __author__    = 'Christoph Kirst <christoph.kirst.ck@gmail.com>'
 __license__   = 'GPLv3 - GNU General Pulic License v3 (see LICENSE)'
 __copyright__ = 'Copyright © 2020 by Christoph Kirst'
-__webpage__   = 'http://idisco.info'
-__download__  = 'http://www.github.com/ChristophKirst/ClearMap2'
+__webpage__   = 'https://idisco.info'
+__download__  = 'https://www.github.com/ChristophKirst/ClearMap2'
+
+from importlib import import_module
+
+
+def lazy_mixin(class_path):
+    """
+    Injects mixin methods on first access, then self-destructs.
+    The mixin module (and its deps like matplotlib) is never
+    imported until someone actually calls a mixed-in method.
+
+    Arguments
+    ---------
+    class_path : str
+        The dotted path to the mixin class, e.g. "ClearMap.Visualization.Plotting.PlottingMixin".
+    """
+    module_path, mixin_name = class_path.rsplit('.', 1)
+
+    def decorator(cls):
+        def __getattr__(self, name):
+            mixin = getattr(import_module(module_path), mixin_name)
+            for k, v in vars(mixin).items():
+                if not k.startswith('_'):
+                    setattr(cls, k, v)           # patch the class, not the instance
+            del cls.__getattr__                  # self-destruct
+            return getattr(self, name)           # retry (or AttributeError if truly missing)
+
+        cls.__getattr__ = __getattr__
+        return cls
+
+    return decorator
 
 
 class lazyattr:
